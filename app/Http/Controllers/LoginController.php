8e0c31c12122ce3c453a53c6f25a8dce0bd3724e
ビビ <38562978+0xOptimizer@users.noreply.github.com>
2025-02-09 09:02:04 +0800

    update: remove js facade

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,17 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
-use Laracasts\Utilities\JavaScript\JavaScriptFacade;
 
 class LoginController extends Controller
 {
     function index() {
-        JavaScriptFacade::put([
-            'urlBase' => URL::to('/'),
-            'currentUrl' => URL::full(),
-            'assetUrl' => asset('/')
-        ]);
-
         $data = [
             'title' => 'Login'
         ];
